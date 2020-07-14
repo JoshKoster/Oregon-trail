b340ff70476c9b5c7ba16483ccb3c425c12efb06
JoshKoster <62957883+JoshKoster@users.noreply.github.com>
2020-07-14 12:51:31 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,23 +1,28 @@
 class Wagon {
-    constructor(capacity){
+    constructor(capacity) {
         this.capacity = capacity
         this.passengers = []
 
     }
-    getAvailableSeatCount(){
+    getAvailableSeatCount() {
         return this.capacity - this.passengers.length
     }
-    join(traveler){
-        if(this.getAvailableSeatCount() > 0)
-        return this.passengers.push(traveler)
+    join(traveler) {
+        if (this.getAvailableSeatCount() > 0) {
+            this.passengers.push(traveler)
+        }
     }
-    shouldQuarantine (){
-        const quarantine = this.passengers.some(quaran => quaran.isHealhy === false)
+    shouldQuarantine() {
+        let quarantine = this.passengers.some(traveler => traveler.isHealhy === false)
         return quarantine
     }
-    totalFood(){
-        const foodAdded = this.passengers.reduce ((a)=> a.food)
-        return foodAdded
+    totalFood() {
+        let foodSum = this.passengers.reduce((sum, travler) => {
+            let result = sum + traveler.food
+            console.log(result)
+            return result
+        }, 0)
+        return foodSum
     }
 
 
@@ -33,24 +38,28 @@ class Traveler {
         return this.food = this.food + 2
     }
     eat() {
-        this.food = this.food - 1 : this.isHealthy = false
-        return (this.food > 0)
+        if (this.food > 0) {
+            this.food = 1
+        } else {
+            this.isHealhy = false
+        }
     }
 }
+
 let wagon = new Wagon(2)
 // Create three travelers
 let henrietta = new Traveler('Henrietta')
 let juan = new Traveler('Juan')
 let maude = new Traveler('Maude')
-console.log(`Wagon Seat Count?: ${ wagon.getAvailableSeatCount() } – EXPECTED: 2. The wagon starts with 2 seats. We haven't added travelers to the wagon yet.`)
+console.log(`Wagon Seat Count?: ${wagon.getAvailableSeatCount()} – EXPECTED: 2. The wagon starts with 2 seats. We haven't added travelers to the wagon yet.`)
 wagon.join(henrietta)
-console.log(`Wagon Seat Count?: ${ wagon.getAvailableSeatCount() } – EXPECTED: 1. Henrietta just joined.`)
+console.log(`Wagon Seat Count?: ${wagon.getAvailableSeatCount()} – EXPECTED: 1. Henrietta just joined.`)
 wagon.join(juan)
 wagon.join(maude)  // There is no room for her!
-console.log(`Wagon Seat Count?: ${ wagon.getAvailableSeatCount() } – EXPECTED: 0 – There is no room for Maude, but Juan was able to join.`)
+console.log(`Wagon Seat Count?: ${wagon.getAvailableSeatCount()} – EXPECTED: 0 – There is no room for Maude, but Juan was able to join.`)
 henrietta.hunt()   // Henrietta goes in search of food.
 juan.eat()         // Juan eats – as Juan does. 🤣
 juan.eat()         // Juan has run out of food!
 console.log(juan)
-console.log(`Wagon Should Quarantine?: ${ wagon.shouldQuarantine() } – EXPECTED: true. Juan has run out of food and become unhealthy!`)
-console.log(`Wagon's Total Food?: ${ wagon.totalFood() } – EXPECTED: 3.`)
+console.log(`Wagon Should Quarantine?: ${wagon.shouldQuarantine()} – EXPECTED: true. Juan has run out of food and become unhealthy!`)
+console.log(`Wagon's Total Food?: ${wagon.totalFood()} – EXPECTED: 3.`)
